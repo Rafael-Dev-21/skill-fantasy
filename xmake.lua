42cb466033dfd39bdb5f14494586155b14eafe90
Rafael-Dev-21 <victorrafaelpereiraalves0202@gmail.com>
2022-10-30 18:10:09 -0300

    Hoje foi o dia dos sistemas de build
    Xmake

---
+++ xmake.lua
@@ -0,0 +1,13 @@
+target("skfantasy")
+	set_kind("binary")
+	add_files("src/*.c")
+	add_includedirs("$(projectdir)/include")
+	if is_plat("windows") then
+		add_installfiles("$(projectdir)/pdcurses.dll")
+		add_deps("pdcurses")
+		add_links("pdcurses")
+	else
+		add_links("ncurses")
+	end
+
+	add_links("m")
