5b2a228ec26d7f3c7f803e95e52d77075bb0d7e4
Rafael-Dev-21 <victorrafaelpereiraalves0202@gmail.com>
2023-01-16 16:09:29 -0300

    Teste de workflow

--- xmake.lua
+++ xmake.lua
@@ -1,4 +1,12 @@
+if is_plat("mingw") then
+  add_requires("pdcurses", {configs = {port = "wincon"}})
+end
+
 target("skfantasy")
   set_kind("binary")
   add_files("src/*.cpp")
-  add_links("ncurses")
+  if is_plat("mingw") then
+    add_links("pdcurses")
+  else
+    add_links("ncurses")
+  end
