63e81e80215b2318f4cf6740abe5d48531d2f76b
Rafael-Dev-21 <victorrafaelpereiraalves0202@gmail.com>
2023-01-16 16:49:40 -0300

    Adicionados links estáticos a versão windows

--- xmake.lua
+++ xmake.lua
@@ -7,6 +7,7 @@ target("skfantasy")
   add_files("src/*.cpp")
   if is_plat("mingw") then
     add_packages("pdcurses")
+    add_ldflags("-static-libgcc", "-static-libstdc++")
   else
     add_links("ncurses")
   end
