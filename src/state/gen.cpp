f8d5cb8c6b301e4691dd1cd2d9a187d29fd80518
Rafael-Dev-21 <victorrafaelpereiraalves0202@gmail.com>
2023-08-12 02:22:11 -0300

    added mingw-std-threads submodule

--- src/state/gen.cpp
+++ src/state/gen.cpp
@@ -1,7 +1,12 @@
 #include <cmath>
 #include <ctime>
 #include <chrono>
+
+#ifdef __MINGW32__
+#include <mingw.thread.h>
+#else
 #include <thread>
+#endif
 
 #include <curses.h>
 
--- xmake.lua
+++ xmake.lua
@@ -10,12 +10,8 @@ target("skfantasy")
 	add_includedirs("include")
 	if is_plat("windows") or is_plat("mingw") then
 		add_packages("pdcurses")
-        add_cxxflags("-static-libgcc",
-                     "-static-libgcc_s_seh-1",
-                     "-static-libstdc++",
-                     "-static-libstdc++-6",
-                     "-static-libwinpthread",
-                     "-static-libwinpthread-1")
+        add_cxxflags("-static-libgcc", "-static-libstdc++")
+        add_includedirs("vendor/mingw-std-threads")
 	else
 		add_links("ncurses")
 	end
