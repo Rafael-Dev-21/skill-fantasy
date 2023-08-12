9992c7b8031334f2252234a3cf15b59d68a44760
Rafael-Dev-21 <victorrafaelpereiraalves0202@gmail.com>
2023-08-12 04:54:23 -0300

    sdl2 pdcurses

--- xmake.lua
+++ xmake.lua
@@ -1,7 +1,7 @@
 add_rules("mode.debug", "mode.release")    
 
 if is_plat("windows") or is_plat("mingw") then
-	add_requires("pdcurses", { configs = { port = "wincon" } })
+	add_requires("pdcurses")
 end
 
 target("skfantasy")
