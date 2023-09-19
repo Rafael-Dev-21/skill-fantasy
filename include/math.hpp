330729a15daf511d8714a4b0d742eaec0808d363
Victor Rafael <victorrafaelpereiraalves0202@gmail.com>
2023-09-19 12:08:58 -0300

    changed drand48 to rand

--- include/math.hpp
+++ include/math.hpp
@@ -3,12 +3,6 @@
 #include <cmath>
 #include <cstdlib>
 
-#ifdef __MINGW32__
-export "C" {
-  #include <drand48.h>
-};
-#endif
-
 namespace Math {
 float lerp(float a, float b, float t);
 float smooth(float t);
@@ -19,7 +13,7 @@ float hash(int x, int y);
 template <typename T>
 static void shuffle(T* array, int size) {
   for (int e = size - 1; e > 0; e--) {
-    int index = (int)round(drand48() * (e - 1));
+    int index = rand() % e;
     T temp = array[e];
 
     array[e] = array[index];
