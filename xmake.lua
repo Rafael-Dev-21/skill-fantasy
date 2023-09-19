fd1461c069a575ff5d7443f9169e8747a63c1c66
Victor Rafael <victorrafaelpereiraalves0202@gmail.com>
2023-09-18 23:33:59 -0300

    formatting corrections

--- xmake.lua
+++ xmake.lua
@@ -1,11 +1,17 @@
 add_rules("mode.debug", "mode.release")
 
-        if is_plat ("windows") or
-    is_plat("mingw") then add_requires("pdcurses") end
+if is_plat ("windows") or is_plat("mingw") then
+  add_requires("pdcurses")
+end
 
-    target("skfantasy") set_kind("binary") add_files("src/**.cpp")
-        add_includedirs("include") if is_plat ("windows") or
-    is_plat("mingw") then add_packages("pdcurses")
-        add_includedirs("mingw-std-threads")
-            add_ldflags("-static-libgcc",
-                        "-static-libstdc++") else add_links("ncurses") end
+target("skfantasy")
+  set_kind("binary")
+  add_files("src/**.cpp")
+  add_includedirs("include")
+  if is_plat ("windows") or is_plat("mingw") then
+    add_packages("pdcurses")
+    add_includedirs("mingw-std-threads")
+    add_ldflags("-static-libgcc", "-static-libstdc++")
+  else
+    add_links("ncurses")
+  end
