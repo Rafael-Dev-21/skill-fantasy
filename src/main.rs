dffd2b2bdd940e96c38a54930fff39cb171c1d33
Rafael 04th <victorrafaelpereiraalves0202@gmail.com>
2026-04-10 13:55:21 -0300

    Borders!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+struct Player {
+    x: i32,
+    y: i32,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let width = 40;
+    let height = 25;
+
+    let p = Player {
+        x: width / 2,
+        y: height / 2,
+    };
+
+    for y in -1..=height {
+        for x in -1..=width {
+            if x == -1 && y == -1 {
+                print!("+");
+            } else if x == width && y == -1 {
+                print!("+");
+            } else if x == -1 && y == height {
+                print!("+");
+            } else if x == width && y == height {
+                print!("+");
+            } else if x == -1 || x == width {
+                print!("|");
+            } else if y == -1 || y == height {
+                print!("—");
+            } else if x == p.x && y == p.y {
+                print!("@");
+            } else {
+                print!(".");
+            }
+        }
+        print!("\n");
+    }
 }
